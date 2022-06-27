a9f419ec3910fffb009a6cac930d879c42297754
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2022-06-27 23:12:21 +0200

    Add Hack Nerd Font for nerd font stuff

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static char *font = "JetBrains Mono:size=12";
 /* Spare fonts */
 static char *font2[] = {
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
 };
 #endif // FONT2_PATCH
 
